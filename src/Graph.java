8921ff55e1934155505c75d5582413da9afcf951
DevinParentice <devinparentice@gmail.com>
2021-12-07 16:49:14 -0500

    Fixed Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -1,29 +1,68 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import java.io.IOException;
+// import org.jsoup.Jsoup;
+// import org.jsoup.nodes.Document;
+// import org.jsoup.nodes.Element;
+// import org.jsoup.select.Elements;
+// import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Graph {
-    private ArrayList<Node> nodes = null;
+    public ArrayList<Node> nodes = new ArrayList<Node>();
 
     public boolean contains(String courseNum) {
-        for(Node x: courseList) {
+        for(Node x: nodes) {
             if(x.getCourse().equals(courseNum)) { return true; }
         }
         return false;
     }
 
-	public static addNode(String number, String name, ArrayList<Node> prereqs) {
-		Node course = new Node(number, name, prereqs)
+	public void addNode(String number, String name, ArrayList<Node> prereqs) {
+		Node course = new Node(number, name, prereqs);
 		nodes.add(course);
 	}
 
-    public static void main(String[] args) {
-        System.out.println(new URLParser().getClassList());
-    }
+    public static void main(String args[]) {
+		Graph graph = new Graph();
+		ArrayList<Node> empty = new ArrayList<Node>();
+		graph.addNode("MAT 151", "Introduction to Discrete Mathematics", empty);
+		graph.addNode("MAT 161", "Calculus I", empty);
+		graph.addNode("CSC 141", "Computer Science I", empty);
+		graph.addNode("CSC 142", "Computer Science II", new ArrayList<Node>(Arrays.asList(graph.nodes.get(2))));
+		graph.addNode("CSC 220", "Foundations of Computer Science", new ArrayList<Node>(Arrays.asList(graph.nodes.get(0), graph.nodes.get(1))));
+		graph.addNode("CSC 231", "Computer Systems", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3))));
+		graph.addNode("CSC 240", "Computer Science III", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3))));
+		graph.addNode("CSC 241", "Data Structures and Algorithms", new ArrayList<Node>(Arrays.asList(graph.nodes.get(0), graph.nodes.get(1), graph.nodes.get(6))));
+		graph.addNode("CSC 242", "Computer Organization", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3), graph.nodes.get(0))));
+		graph.addNode("CSC 301", "Computer Security & Ethics", empty);
+		graph.addNode("CSC 335", "Data Communications and Networking I", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7), graph.nodes.get(8))));
+		graph.addNode("CSC 302", "Computer Security", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3), graph.nodes.get(10))));
+		graph.addNode("CSC 317", "Introduction to Digital Image Processing", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3), graph.nodes.get(0), graph.nodes.get(1))));
+		graph.addNode("CSC 321", "Database Management Systems", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7))));
+		graph.addNode("CSC 331", "Operating Systems", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3), graph.nodes.get(6), graph.nodes.get(7))));
+		graph.addNode("CSC 336", "Data Communications and Networking II", new ArrayList<Node>(Arrays.asList(graph.nodes.get(10))));
+		graph.addNode("CSC 345", "Programming Language Concepts and Paradigms", new ArrayList<Node>(Arrays.asList(graph.nodes.get(4), graph.nodes.get(7))));
+		graph.addNode("CSC 400", "Internship", new ArrayList<Node>(Arrays.asList(graph.nodes.get(6), graph.nodes.get(7))));
+		graph.addNode("CSC 402", "Software Engineering", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7))));
+		graph.addNode("CSC 404", "Software Engineering & Testing", new ArrayList<Node>(Arrays.asList(graph.nodes.get(6), graph.nodes.get(7))));
+		graph.addNode("CSC 416", "Design and Construction of Compilers", new ArrayList<Node>(Arrays.asList(graph.nodes.get(4), graph.nodes.get(6), graph.nodes.get(7))));
+		graph.addNode("CSC 417", "User Interfaces", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7))));
+		graph.addNode("CSC 466", "Distributed and Parallel Programming", new ArrayList<Node>(Arrays.asList(graph.nodes.get(5))));
+		graph.addNode("CSC 468", "Introduction to Cloud Computing", new ArrayList<Node>(Arrays.asList(graph.nodes.get(5))));
+		graph.addNode("CSC 471", "Modern Malware Analysis", new ArrayList<Node>(Arrays.asList(graph.nodes.get(8))));
+		graph.addNode("CSC 472", "Software Security", new ArrayList<Node>(Arrays.asList(graph.nodes.get(8))));
+		graph.addNode("CSC 476", "Game Development", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7))));
+		graph.addNode("CSC 481", "Artificial Intelligence", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7))));
+		graph.addNode("CSC 490", "Independent Project", new ArrayList<Node>(Arrays.asList(graph.nodes.get(4), graph.nodes.get(7))));
+		graph.addNode("CSC 495", "Topics in Computer Science", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7))));
+		graph.addNode("CSC 496", "Topics in Complex Systems", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7))));
+		graph.addNode("CSC 497", "Topics in Computer Security", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7))));
+		graph.addNode("CSC 499", "Independent Study in Computer Science", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7))));
+
+		for (Node x: graph.nodes) {
+			System.out.println(x.getCourse() + " -- " + x.getName() + "\n\t" + x.getPrereqs());
+		}
+	}
 }
 
 enum Offerings {
@@ -64,14 +103,28 @@ class Node {
 */
 
 class Node {
-	public Node(this, String number, String name, ArrayList<Node> prereqs) {
+	String number;
+	String name;
+	ArrayList<Node> prereqs;
+	public Node(String number, String name, ArrayList<Node> prereqs) {
 		this.number = number;
 		this.name = name;
-		this.prereqs = neighbors
+		this.prereqs = prereqs;
 	}
 
+	public String getCourse() {
+		return this.number;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public ArrayList<Node> getPrereqs() {
+		return this.prereqs;
+	}
 	public String toString() {
-		return this.number + "----" + this.name
+		return this.number;
 	}
 }
 /*
@@ -118,40 +171,3 @@ class URLParser {
     }
 }
 */
-
-public static void main(String args[]) {
-	Graph graph = new Graph();
-	graph.add_node("MAT 151", "Introduction to Discrete Mathematics");
-	graph.add_node("MAT 161", "Calculus I");
-	graph.add_node("CSC 141", "Computer Science I");
-	graph.add_node("CSC 142", "Computer Science II", [graph.nodes.get(4)]);
-	graph.add_node("CSC 220", "Foundations of Computer Science", [graph.nodes.get(0), graph.nodes.get(1)]);
-	graph.add_node("CSC 231", "Computer Systems", [graph.nodes.get(4)]);
-	graph.add_node("CSC 240", "Computer Science III", [graph.nodes.get(4)]);
-	graph.add_node("CSC 241", "Data Structures and Algorithms", [graph.nodes.get(0), graph.nodes.get(1), graph.nodes.get(4)]);
-	graph.add_node("CSC 242", "CSC 242", "Computer Organization", [graph.nodes.get(4), graph.nodes.get(0)]);
-	graph.add_node("CSC 301", "Computer Security & Ethics");
-	graph.add_node("CSC 335", "Data Communications and Networking I", [graph.nodes.get(4), graph.nodes.get(7)]);
-	graph.add_node("CSC 302", "Computer Security", [graph.nodes.get(4), graph.nodes[10]]);
-	graph.add_node("CSC 317", "Introduction to Digital Image Processing", [graph.nodes.get(4), graph.nodes.get(0), graph.nodes.get(1)]);
-	graph.add_node("CSC 321", "Database Management Systems", [graph.nodes.get(7)]);
-	graph.add_node("CSC 331", "Operating Systems", [graph.nodes.get(4), graph.nodes.get(4), graph.nodes.get(7)]);
-	graph.add_node("CSC 336", "Data Communications and Networking II", [graph.nodes[10]]);
-	graph.add_node("CSC 345", "Programming Language Concepts and Paradigms", [graph.nodes.get(4), graph.nodes.get(7)]);
-	graph.add_node("CSC 400", "Internship", [graph.nodes.get(4), graph.nodes.get(7)]);
-	graph.add_node("CSC 402", "Software Engineering", [graph.nodes.get(7)]);
-	graph.add_node("CSC 404", "Software Engineering & Testing", [graph.nodes.get(4), graph.nodes.get(7)]);
-	graph.add_node("CSC 416", "Design and Construction of Compilers", [graph.nodes.get(4), graph.nodes.get(4), graph.nodes.get(7)]);
-	graph.add_node("CSC 417", "User Interfaces", [graph.nodes.get(7)]);
-	graph.add_node("CSC 466", "Distributed and Parallel Programming", [graph.nodes.get(5)]);
-	graph.add_node("CSC 468", "Introduction to Cloud Computing", [graph.nodes.get(5)]);
-	graph.add_node("CSC 471", "Modern Malware Analysis", [graph.nodes[8]]);
-	graph.add_node("CSC 472", "Software Security", [graph.nodes[8]]);
-	graph.add_node("CSC 476", "Game Development", [graph.nodes.get(7)]);
-	graph.add_node("CSC 481", "Artificial Intelligence", [graph.nodes.get(7)]);
-	graph.add_node("CSC 490", "Independent Project", [graph.nodes.get(4), graph.nodes.get(7)]);
-	graph.add_node("CSC 495", "Topics in Computer Science", [graph.nodes.get(7)]);
-	graph.add_node("CSC 496", "Topics in Complex Systems", [graph.nodes.get(7)]);
-	graph.add_node("CSC 497", "Topics in Computer Security", [graph.nodes.get(7)]);
-	graph.add_node("CSC 499", "Independent Study in Computer Science", [graph.nodes.get(7)]);
-}
