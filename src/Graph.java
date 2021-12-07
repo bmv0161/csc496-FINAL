a5bcb04904ef38e7bde69dedce0a2ec6e409a316
Devin Parentice <devinparentice@gmail.com>
2021-12-07 03:13:40 -0500

    Added data to graph

--- src/Graph.java
+++ src/Graph.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 
 public class Graph {
-    private ArrayList<Node> courseList = null;
+    private ArrayList<Node> nodes = null;
 
     public boolean contains(String courseNum) {
         for(Node x: courseList) {
@@ -15,6 +15,11 @@ public class Graph {
         }
         return false;
     }
+
+	public static addNode(String number, String name, ArrayList<Node> prereqs) {
+		Node course = new Node(number, name, prereqs)
+		nodes.add(course);
+	}
 
     public static void main(String[] args) {
         System.out.println(new URLParser().getClassList());
@@ -24,7 +29,7 @@ public class Graph {
 enum Offerings {
     WINTER, SPRING, SUMMER, FALL
 }
-
+/*
 class Node {
     private String courseNum;
     private ArrayList<Node> prereq = null;
@@ -41,7 +46,7 @@ class Node {
     public void setPrereq(ArrayList<Node> prereq) {
         this.prereq = prereq;
     }
-    public void addPrereq(Node course) {
+    public void addPrereq(String course) {
         this.prereq.add(course);
     }
 
@@ -56,7 +61,20 @@ class Node {
         return this.prereq;
     }
 }
+*/
 
+class Node {
+	public Node(this, String number, String name, ArrayList<Node> prereqs) {
+		this.number = number;
+		this.name = name;
+		this.prereqs = neighbors
+	}
+
+	public String toString() {
+		return this.number + "----" + this.name
+	}
+}
+/*
 class URLParser {
     private final String url =
             "https://www.wcupa.edu/sciences-mathematics/computerScience/undergradCourses.aspx";
@@ -98,4 +116,44 @@ class URLParser {
         }
         return courseList;
     }
+}
+*/
+
+public static void main(String args[]) {
+	Graph graph = new Graph();
+	graph.add_node("MAT 151", "Introduction to Discrete Mathematics");
+	graph.add_node("MAT 161", "Calculus I");
+	graph.add_node("CSC 141", "Computer Science I");
+	graph.add_node("CSC 142", "Computer Science II", [graph.nodes[2]]);
+	graph.add_node("CSC 220", "Foundations of Computer Science", [graph.nodes[0], graph.nodes[1]]);
+	graph.add_node("CSC 231", "Computer Systems", [graph.nodes[4]]);
+	graph.add_node("CSC 240", "Computer Science III", [graph.nodes[4]]);
+	graph.add_node("CSC 241", "Data Structures and Algorithms", [graph.nodes[0], graph.nodes[1], graph.nodes[6]]);
+	graph.add_node("CSC 242", "CSC 242", "Computer Organization", [graph.nodes[4], graph.nodes[0]]);
+	graph.add_node("CSC 301", "Computer Security & Ethics");
+	graph.add_node("CSC 335", "Data Communications and Networking I", [graph.nodes[6], graph.nodes[7]]);
+	graph.add_node("CSC 302", "Computer Security", [graph.nodes[9], graph.nodes[10]]);
+	graph.add_node("CSC 317", "Introduction to Digital Image Processing", [graph.nodes[6], graph.nodes[0], graph.nodes[1]]);
+	graph.add_node("CSC 321", "Database Management Systems", [graph.nodes[7]]);
+	graph.add_node("CSC 331", "Operating Systems", [graph.nodes[4], graph.nodes[6], graph.nodes[7]]);
+	graph.add_node("CSC 336", "Data Communications and Networking II", [graph.nodes[10]]);
+	graph.add_node("CSC 345", "Programming Language Concepts and Paradigms", [graph.nodes[4], graph.nodes[7]]);
+	graph.add_node("CSC 400", "Internship", [graph.nodes[6], graph.nodes[7]]);
+	graph.add_node("CSC 402", "Software Engineering", [graph.nodes[7]]);
+	graph.add_node("CSC 404", "Software Engineering & Testing", [graph.nodes[6], graph.nodes[7]]);
+	graph.add_node("CSC 416", "Design and Construction of Compilers", [graph.nodes[4], graph.nodes[6], graph.nodes[7]]);
+	graph.add_node("CSC 417", "User Interfaces", [graph.nodes[7]]);
+	graph.add_node("CSC 466", "Distributed and Parallel Programming", [graph.nodes[5]]);
+	graph.add_node("CSC 468", "Introduction to Cloud Computing", [graph.nodes[5]]);
+	graph.add_node("CSC 471", "Modern Malware Analysis", [graph.nodes[8]]);
+	graph.add_node("CSC 472", "Software Security", [graph.nodes[8]]);
+	graph.add_node("CSC 476", "Game Development", [graph.nodes[7]]);
+	graph.add_node("CSC 481", "Artificial Intelligence", [graph.nodes[7]]);
+	graph.add_node("CSC 490", "Independent Project", [graph.nodes[4], graph.nodes[7]]);
+	graph.add_node("CSC 495", "Topics in Computer Science", [graph.nodes[7]]);
+	graph.add_node("CSC 496", "Topics in Complex Systems", [graph.nodes[7]]);
+	graph.add_node("CSC 497", "Topics in Computer Security", [graph.nodes[7]]);
+	graph.add_node("CSC 499", "Independent Study in Computer Science", [graph.nodes[7]]);
+
+
 }
