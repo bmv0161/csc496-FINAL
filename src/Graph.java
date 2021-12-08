999489d09003a4aaab4fa94079497c628df0fda8
bmv0161 <bmv0161@gmail.com>
2021-12-07 20:08:20 -0500

    clean up Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -1,40 +1,30 @@
-// import org.jsoup.Jsoup;
-// import org.jsoup.nodes.Document;
-// import org.jsoup.nodes.Element;
-// import org.jsoup.select.Elements;
-// import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 
 public class Graph {
-    public ArrayList<Node> nodes = new ArrayList<Node>();
+    public ArrayList<Node> nodes = new ArrayList<>();
 
-    public boolean contains(String courseNum) {
-        for(Node x: nodes) {
-            if(x.getCourse().equals(courseNum)) { return true; }
-        }
-        return false;
-    }
-
+	public void addNode(String number, String name) {
+		nodes.add(new Node(number, name));
+	}
 	public void addNode(String number, String name, ArrayList<Node> prereqs) {
-		Node course = new Node(number, name, prereqs);
-		nodes.add(course);
+		nodes.add(new Node(number, name, prereqs));
 	}
 
     public static void main(String args[]) {
 		Graph graph = new Graph();
-		ArrayList<Node> empty = new ArrayList<Node>();
-		graph.addNode("MAT 151", "Introduction to Discrete Mathematics", empty);
-		graph.addNode("MAT 161", "Calculus I", empty);
-		graph.addNode("CSC 141", "Computer Science I", empty);
+		//ArrayList<Node> empty = new ArrayList<Node>();
+		graph.addNode("MAT 151", "Introduction to Discrete Mathematics");
+		graph.addNode("MAT 161", "Calculus I");
+		graph.addNode("CSC 141", "Computer Science I");
 		graph.addNode("CSC 142", "Computer Science II", new ArrayList<Node>(Arrays.asList(graph.nodes.get(2))));
 		graph.addNode("CSC 220", "Foundations of Computer Science", new ArrayList<Node>(Arrays.asList(graph.nodes.get(0), graph.nodes.get(1))));
 		graph.addNode("CSC 231", "Computer Systems", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3))));
 		graph.addNode("CSC 240", "Computer Science III", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3))));
 		graph.addNode("CSC 241", "Data Structures and Algorithms", new ArrayList<Node>(Arrays.asList(graph.nodes.get(0), graph.nodes.get(1), graph.nodes.get(6))));
 		graph.addNode("CSC 242", "Computer Organization", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3), graph.nodes.get(0))));
-		graph.addNode("CSC 301", "Computer Security & Ethics", empty);
+		graph.addNode("CSC 301", "Computer Security & Ethics");
 		graph.addNode("CSC 335", "Data Communications and Networking I", new ArrayList<Node>(Arrays.asList(graph.nodes.get(7), graph.nodes.get(8))));
 		graph.addNode("CSC 302", "Computer Security", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3), graph.nodes.get(10))));
 		graph.addNode("CSC 317", "Introduction to Digital Image Processing", new ArrayList<Node>(Arrays.asList(graph.nodes.get(3), graph.nodes.get(0), graph.nodes.get(1))));
@@ -68,44 +58,17 @@ public class Graph {
 enum Offerings {
     WINTER, SPRING, SUMMER, FALL
 }
-/*
-class Node {
-    private String courseNum;
-    private ArrayList<Node> prereq = null;
-
-    public Node() {
-    }
-    public Node(String course) {
-        this.courseNum = course;
-    }
-
-    public void setCourseNum(String courseNum) {
-        this.courseNum = courseNum;
-    }
-    public void setPrereq(ArrayList<Node> prereq) {
-        this.prereq = prereq;
-    }
-    public void addPrereq(String course) {
-        this.prereq.add(course);
-    }
-
-    public String toString() {
-        return courseNum + " - " + prereq;
-    }
-
-    public String getCourse() {
-        return this.courseNum;
-    }
-    public ArrayList<Node> getPrereq() {
-        return this.prereq;
-    }
-}
-*/
 
 class Node {
 	String number;
 	String name;
 	ArrayList<Node> prereqs;
+
+	public Node(String number, String name) {
+		this.number = number;
+		this.name = name;
+		prereqs = new ArrayList<Node>();
+	}
 	public Node(String number, String name, ArrayList<Node> prereqs) {
 		this.number = number;
 		this.name = name;
@@ -127,47 +90,3 @@ class Node {
 		return this.number;
 	}
 }
-/*
-class URLParser {
-    private final String url =
-            "https://www.wcupa.edu/sciences-mathematics/computerScience/undergradCourses.aspx";
-    private Element table = null;
-    private ArrayList<Node> courseList = new ArrayList<Node>();
-
-    public URLParser() {
-        try {
-            Document doc = Jsoup.connect(url).get();
-            table = doc.select("table.responsiveTable").get(0);
-            //System.out.println(doc.outerHtml());
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    private void parseTable() {
-        for(Element row: table.select("tr")) {
-            Node course = new Node();
-            for(Element col: row.select("td")) {
-                if(col.attr("data-label").equals("Course Number")) {
-                    Elements link = col.select("a");
-                    String label = link.text();
-                    course.setCourseNum(label);
-                }else if(col.attr("data-label").equals("Prerequisites")) {
-                    String prereq = col.text();
-
-                    //System.out.println(col.);
-                }
-                courseList.add(course);
-
-            }
-        }
-    }
-
-    public ArrayList<Node> getClassList() {
-        if(courseList.isEmpty()) {
-            parseTable();
-        }
-        return courseList;
-    }
-}
-*/
